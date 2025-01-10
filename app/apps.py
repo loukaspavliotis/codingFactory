6ab8101b4380cea952078dbbd4f8b57509ff9d6f
loukaspavliotis <codingloukaspavliotis@gmail.com>
2025-01-11 00:42:32 +0200

    one

--- app/apps.py
+++ app/apps.py
@@ -1,17 +1,29 @@
 from django.apps import AppConfig
 
 
+# class AppConfig(AppConfig):
+#     name = 'app'
+#
+#
+# from django.db import models
+# from django.contrib.auth.models import User
+#
+#
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     bio = models.TextField(blank=True, null=True)
+#
+#     def __str__(self):
+#         return self.user.username
+#
+#
+from django.apps import AppConfig
+
+
 class AppConfig(AppConfig):
     name = 'app'
 
-
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.user.username
+    def ready(self):
+        # If you need to perform specific actions when the app is ready,
+        # you can include that logic here (e.g., signals or imports).
+        pass
